49766eaf840d7d2bac5b54e8d0b4f701802a682e
a.panasenko <andrii.panasenko295@gmail.com>
2024-05-14 23:24:43 +0300

    change tab sizing

--- webpack.config.js
+++ webpack.config.js
@@ -4,31 +4,31 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: 'src/index.html' }),
+        new MiniCssExtractPlugin({ filename: 'styles.css' }),
+        new CleanWebpackPlugin(),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: 'src/index.html' }),
-    new MiniCssExtractPlugin({ filename: 'styles.css' }),
-    new CleanWebpackPlugin(),
-  ],
-  devServer: {
-    open: true,
-    port: 3030,
-  },
+    devServer: {
+        open: true,
+        port: 3030,
+    },
 };
